3f26755b0e1dd1eb09542ba8072f2974d86e8020
KavenSky <1713919065@qq.com>
2026-06-27 15:37:54 +0800

    更新

---
+++ scripts/1.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+unset PYTHONPATH
+unset CONDA_PREFIX CONDA_DEFAULT_ENV CONDA_PYTHON_EXE CONDA_EXE
+
+source /opt/ros/jazzy/setup.bash
+source "${SCRIPT_DIR}/../install/setup.bash"
+
+# 传感器和算法
+# ros2 launch lslidar_driver lslidar_cx_launch.py &
+# ros2 launch camera uav_system.launch.py &
+# ros2 launch vision vision_tracking_system.launch.py &
+ros2 launch uav_mavros2 uav_launch.py &
+# 导航
+# ros2 launch navigation navigation.launch.py &
+
+
+
+wait
